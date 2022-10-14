d052b8965fb28b029c9ea05a78e573eb02e720a6
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-10-14 23:30:04 +0100

    up

--- user_data.php
+++ user_data.php
@@ -1,3 +1,14 @@
 <?php
 
-print_r($_POST);
+// print_r($_POST);
+// $name = $_POST('name');
+// $email = $_POST('email');
+// $DOB = $_POST('dob');
+// $Gender = $_POST('gender');
+// $Country = $_POST('country');
+
+$filename = './userdata.csv';
+$handle = fopen($filename, "r");
+$content = fread($handle, filesize($filename));
+echo $content;
+fclose($handle);
