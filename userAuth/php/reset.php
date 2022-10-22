7425596387e2edd76e81a1fa5e8d4a3adc8279ef
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-22 01:30:04 +0100

    Update reset.php

--- userAuth/php/reset.php
+++ userAuth/php/reset.php
@@ -1,15 +1,16 @@
 <?php
 if(isset($_POST['submit'])){
-    $email = //complete this;
-    $newpassword = //complete this;
+    $email = $_POST['email']; //complete this;
+    $newpassword = $_POST['password'];//complete this;
 
     resetPassword($email, $password);
 }
 
 function resetPassword($email, $password){
     //open file and check if the username exist inside
+    $filename = "./users.csv";
+    $handle = fopen($filename, "w");
+    
     //if it does, replace the password
 }
 echo "HANDLE THIS PAGE";
-
-
