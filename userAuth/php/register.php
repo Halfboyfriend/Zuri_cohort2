d09ddb03a06739b5d26cd1ff47bf516ee397c36d
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-22 01:29:11 +0100

    Update register.php

--- userAuth/php/register.php
+++ userAuth/php/register.php
@@ -10,8 +10,13 @@ registerUser($username, $email, $password);
 
 function registerUser($username, $email, $password){
     //save data into the file
+    $filename = "./users.csv";
+    $handle = fopen($filename, "r");
+    $content = fread($handle, filesize($filename));
+    fclose($handle);
     
     // echo "OKAY";
+    echo "OKAY";
 }
 echo "HANDLE THIS PAGE";
 
