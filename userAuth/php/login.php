0fbea168e7ba33fd975ecd466b3c8c09b34c4460
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-22 01:28:17 +0100

    Update login.php

--- userAuth/php/login.php
+++ userAuth/php/login.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['submit'])){
-    $username = //finish this line
-    $password = //finish this
+    $username = $_POST['name']; //finish this line
+    $password = $_POST['password'];   //finish this
 
 loginUser($email, $password);
 
@@ -12,6 +12,11 @@ function loginUser($email, $password){
         Finish this function to check if username and password 
     from file match that which is passed from the form
     */
+    if ($username == $_POST['email'] && $password == $_POST['password'])
+    {
+        loginUser($email, $password);
+        session_start();
+    }
 }
 
 echo "HANDLE THIS PAGE";
