8784ce62566ad9056c415601d2c663851b8d3ebf
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-22 22:34:20 +0100

    Update logout.php

--- userAuth/php/logout.php
+++ userAuth/php/logout.php
@@ -5,6 +5,12 @@ Check if the existing user has a session
 if it does
 destroy the session and redirect to login page
 */
+if ($_SESSION['id']){
+    unset($_SESSION['id']);
+}
+header('Location:login.php');
 }
 
+logout();
+//Task Completed
 echo "HANDLE THIS PAGE";
