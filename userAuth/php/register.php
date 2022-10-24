4f0b777998c18cc782f348a05e4bac8cf87cd099
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-24 07:31:57 +0100

    Update register.php

--- userAuth/php/register.php
+++ userAuth/php/register.php
@@ -9,16 +9,46 @@ registerUser($username, $email, $password);
 }
 
 function registerUser($username, $email, $password){
-    //save data into the file
-    $filename = "./users.csv";
-    $handle = fopen($filename, "a");
+    $data = [$username, $email, $password];
+
+
+    $file = "../storage/users.csv";
+    $file = fopen($file, "r");
+    $readCsv = fgetcsv($file);
+
+    if($readCsv[1] == $data[1])
+    {
+        
+        $message = "User Already Exist";
+
+        echo "<script>
+        alert('$message');
+        window.location.href='../forms/register.html';
+        </script>";
+    } else
+    {
+         //save data into the file
+    $file = "../storage/users.csv";
+    $handle = fopen($file, "a");
     $data_input = implode(",", $_POST) . "\n";
     fwrite($handle, $data_input);
     fclose($handle);
+
+        //send success message and redirect to login page
+        $message = "Registration Successful";
+
+        echo "<script>
+        alert('$message');
+        window.location.href='../forms/login.html';
+        </script>";
+    }
+
+   
     
-    // echo "OKAY";
-    echo "OKAY";
+   
 
     //Task Completed
 }
-echo "HANDLE THIS PAGE";
+//echo "HANDLE THIS PAGE";
+
+
